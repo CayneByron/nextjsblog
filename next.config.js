7cc32d1be337a851619756f98c8fc3b7abc42678
Cayne Butt <cayne@shoploba.com>
2023-04-28 14:30:50 -0700

    no cache please

--- next.config.js
+++ next.config.js
@@ -4,4 +4,18 @@ module.exports = {
     loader: 'default',
     domains: ['images.ctfassets.net'],
   },
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache",
+          },
+        ],
+      },
+    ];
+  },
 }
