e66532255787b5034e8adfb9ae537ffc1acb22a7
Cayne Butt <cayne@shoploba.com>
2023-04-30 10:51:55 -0700

    Config stuff

--- next.config.js
+++ next.config.js
@@ -3,15 +3,5 @@ module.exports = {
   images: {
     loader: 'default',
     domains: ['images.ctfassets.net'],
-  },
-  reactStrictMode: true,
-  headers: {
-    source: "/(.*)",
-    headers: [
-      {
-        key: "Cache-Control",
-        value: "no-cache",
-      },
-    ],
-  },
+  }
 }
